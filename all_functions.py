aefbd0e99bf6ac2247b46d5cb97c25a681c03696
gemmamanns <gemma.manns@griffithuni.edu.au>
2024-09-22 14:13:08 +1000

    Update all_functions.py

--- all_functions.py
+++ all_functions.py
@@ -20,4 +20,45 @@ def filter_food_by_name(food_name: str, data: pd.DataFrame):
     
     return filtered_data
 
+#function for filtering the results based on a high and low value for a nutrient
+def nutrition_range_filter(user_nutrient_input, user_nutrient_min, user_nutrient_max, data: pd.DataFrame):
 
+    nutrient_column = data[user_nutrient_input]
+    user_nutrient_min = int(min_range)
+    user_nutrient_max = int(max_range)
+    food_arr = []
+    for entry in nutrient_column:
+        if entry < user_nutrient_max and entry > user_nutrient_min:
+            filtered_data.append(True)
+        else:
+            filtered_data.append(False)
+
+    return food_arr
+
+
+#function that gives the high and low values for the levels for the nutrition level filter
+def nutrition_filter_levels(level):
+    percentage_low = 0
+    percentage_high = 0
+    if level == 'high':
+        percentage_low = 67
+        percentage_high = 100
+    if level == 'mid':
+        percentage_low = 34
+        percentage_high = 66
+    if level == 'low':
+        percentage_low = 0
+        percentage_high = 33
+
+    return percentage_low, percentage_high
+
+def nutrition_filter_search_levels(user_nutrient_input, user_nutrient_level, data: pd.DataFrame):
+    percentage_low, percentage_high = nutrition_filter_levels(user_nutrient_level)
+    nutrient_column = data[user_nutrient_input]
+    food_arr = []
+    for entry in nutrient_column:
+            if entry > percentage_low and entry < percentage_high:
+                food_arr.append(True)
+            else:
+                food_arr.append(False)
+    return food_arr
